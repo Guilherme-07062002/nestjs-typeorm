db0d8a91eefadb5f63f2e7b7f104a89ddf802b39
Guilherme-07062002 <guilherme762002@gmail.com>
2024-08-15 20:34:42 -0300

    feat: generate partner module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PartnerModule } from './partner/partner.module';
 
 @Module({
-  imports: [],
+  imports: [PartnerModule],
   controllers: [AppController],
   providers: [AppService],
 })
